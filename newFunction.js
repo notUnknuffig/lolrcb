140153ae25035b2890e742ef541be695c757bf94
notUnknuffig <139910467+notUnknuffig@users.noreply.github.com>
2024-01-26 15:02:08 +0100

    Start programming the functions.

--- newFunction.js
+++ newFunction.js
@@ -1,6 +1,30 @@
-class champion {
-    constructor(name, build) {
-        this.name = name;
-        this.build = build;
+class save {
+    constructor(index, isMeta) {
+        this.index = index;
+        this.isMeta = isMeta;
     }
+}
+
+var data = JSON.parse(championData);
+var backSaves = [save(0, true)];
+var oneTimeSave = save(0, true);
+
+function loadChampion(index, isMeta) {
+    document.getElementById("erandomChampion").innerHTML = "Champion: " + data[0]["name"];
+    document.getElementById("image").src = data[0]["image"];
+}
+
+function rerollChampion() {
+    index = Math.floor(Math.random() * data.length);
+    loadChampion(index, true);
+    backSaves.push(save(index, true));
+    if (backSaves.length >= 10) {
+        backSaves.pop();
+    }
+    console.log(backSaves)
+    console.log(oneTimeSave)
+}
+
+function save() {
+    oneTimeSave = backSaves[0];
 }
